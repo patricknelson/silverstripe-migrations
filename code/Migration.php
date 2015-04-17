44aa797aaf263f1e39a7bfe035fcc834bd440dda
Brendan McKeown <bmckeown@ebay.com>
2015-04-17 00:17:54 -0400

    Added one more method and documentation for all methods.

--- code/Migration.php
+++ code/Migration.php
@@ -20,7 +20,7 @@ abstract class Migration {
 	#######################################
 
 	/**
-	 * Returns true if table exists in the database
+	 * Returns true if the table exists in the database
 	 *
 	 * @param string $table
 	 * @return boolean
@@ -31,7 +31,7 @@ abstract class Migration {
 	}
 
 	/**
-	 * Returns true if a column currently exists in a database table
+	 * Returns true if a column exists in a database table
 	 *
 	 * @param string $table
 	 * @param string $column
@@ -44,7 +44,7 @@ abstract class Migration {
 	}
 
 	/**
-	 * Returns true if an array of columns currently exist on a database table
+	 * Returns true if an array of columns exist on a database table
 	 *
 	 * @param string $table
 	 * @param array $columns
@@ -67,11 +67,13 @@ abstract class Migration {
 	}
 
 	/**
-	 * Drop columns from a database table if they exist
+	 * Drops columns from a database table.
+	 * Returns false if the table or any of the columns do not exist.
+	 * Returns true if the SQL query was executed.
 	 *
 	 * @param string $table
 	 * @param array $columns
-	 * @return boolean true if any query was executed
+	 * @return boolean
 	 */
 	protected static function dropColumnsFromTable($table, array $columns) {
 		$queried = false;
@@ -88,11 +90,13 @@ abstract class Migration {
 	}
 
 	/**
-	 * Add columns to a database table if they don't exist
+	 * Add columns to a database table if they don't exist.
+	 * Returns false if the table does not exist.
+	 * Returns true if the SQL query was executed.
 	 *
 	 * @param string $table
 	 * @param array $columns e.g. ['MyColumn' => 'VARCHAR(255) CHARACTER SET utf8']
-	 * @return boolean true if any query was executed
+	 * @return boolean
 	 */
 	protected static function addColumnsToTable($table, array $columns) {
 		$queried = false;
@@ -109,14 +113,15 @@ abstract class Migration {
 	}
 
 	/**
-	 * Get a single field value from a row in a database table by ID
-	 * For those times when you can't use the ORM
-	 * e.g. the fields have been removed from $db
+	 * Gets the value for a single column in a row from the database by the ID column.
+	 * Useful when a field has been removed from the class' `$db` property,
+	 * and therefore is no longer accessible through the ORM.
+	 * Returns `null` if the table, column or row does not exist.
 	 *
 	 * @param string $table
 	 * @param string $columns
 	 * @param string||int $id
-	 * @return mixed
+	 * @return string
 	 */
 	protected static function getRowValueFromTable($table, $field, $id) {
 		$value = null;
@@ -135,12 +140,45 @@ abstract class Migration {
 	}
 
 	/**
-	 * Set field values for a row in a database table by ID
+	 * Gets the values for multiple rows on a database table by the ID column.
+	 * Useful when fields have been removed from the class' `$db` property,
+	 * and therefore are no longer accessible through the ORM.
+	 * Returns an empty array if the table, any of the columns or the row do not exist.
+	 *
+	 * @param string $table
+	 * @param array $columns
+	 * @param string||int $id
+	 * @return array ['FieldName' => value]
+	 */
+	protected static function getRowValuesFromTable($table, array $fields, $id) {
+		$values = [];
+		if (self::tableColumnsExist($table, $fields)) {
+			$query = new SQLQuery();
+			$query->setFrom($table)->setSelect($fields)->setWhere("ID = $id");
+			$results = $query->execute();
+			if ($results) {
+				foreach ($results as $result) {
+					foreach ($fields as $field) {
+						$values[$field] = $result[$field];
+					}
+					break;
+				}
+			}
+		}
+		return $values;
+	}
+
+	/**
+	 * Sets the values for multiple rows on a database table by the ID column.
+	 * Useful when fields have been removed from the class' `$db` property,
+	 * and therefore are no longer accessible through the ORM.
+	 * Returns false if the table or any of the rows do not exist.
+	 * Returns true if the SQL query was executed.
 	 *
 	 * @param string $table
 	 * @param array $values ['FieldName' => value]
 	 * @param string||int $id
-	 * @return boolean true if query was executed
+	 * @return boolean
 	 */
 	protected static function setRowValuesOnTable($table, array $values, $id) {
 		$queried = false;
--- doc/HelperMethods.md
+++ doc/HelperMethods.md
@@ -1,2 +1,103 @@
 # SilverStripe 3.x Database Migrations
+
 ## Helper Methods
+
+### `(boolean) tableExists(string $table)`
+
+Returns true if a table exists in the database.
+
+```php
+if (self::tableExists('MyDataObject')) { /*.*/ }
+```
+
+### `(boolean) tableColumnExists(string $table, string $column)`
+
+Returns true if a single column exists on a database table.
+
+```php
+if (self::tableColumnExists('MyDataObject', 'FieldName')) { /*.*/ }
+```
+
+### `(boolean) tableColumnsExist(string $table, array $column)`
+
+Returns true if an array of columns exist on a database table.
+
+```php
+if (self::tableColumnsExist('MyDataObject', ['FieldName', 'OtherField'])) { /*.*/ }
+```
+
+### `(array) getTableColumns(string $table)`
+
+Returns an array of columns (and their properties) that exist on a database table.
+
+```php
+$columns = self::getTableColumns('MyDataObject');
+var_dump($columns);
+
+// output
+array {
+	'ID' => 'int(11) not null auto_increment',
+	'Created' => 'datetime',
+	'LastEdited' => 'datetime',
+	'Title' => 'varchar(255) character set utf8 collate utf8_general_ci',
+	'Content' => 'mediumtext character set utf8 collate utf8_general_ci'
+}
+```
+
+### `(boolean) dropColumnsFromTable(string $table, array $columns)`
+
+Drops columns from a database table. Returns false if the table or any of the columns do not exist. Returns true if the SQL query was executed.
+
+```php
+self::dropColumnsFromTable('MyDataObject', ['FieldName', 'OtherField']);
+```
+
+### `(boolean) addColumnsToTable(string $table, array $columns)`
+
+Adds columns with the specified properties to a database table if they don't already exist. Returns false if the table does not exist. Returns true if the SQL query was executed.
+
+```php
+self::addColumnsToTable('MyDataObject', [
+	'FieldName' => 'VARCHAR(255) CHARACTER SET utf8', 
+	'OtherField' => 'INT(11) NOT NULL'
+]);
+```
+
+### `(string) getRowValueFromTable(string $table, string $field, int $id)`
+
+Gets the value for a single column in a row from the database by the ID column. Useful when a field has been removed from the class' `$db` property, and therefore is no longer accessible through the ORM. Returns `null` if the table, column or row does not exist.
+
+```php
+$FieldName = self::getRowValueFromTable('MyDataObject', 'FieldName', 15);
+var_dump($FieldName);
+
+// output
+string 'Foo'
+```
+
+### `(array) getRowValuesFromTable(string $table, array $fields, int $id)`
+
+Gets the values for multiple rows on a database table by the ID column. Useful when fields have been removed from the class' `$db` property, and therefore are no longer accessible through the ORM. Returns an empty array if the table, any of the columns or the row do not exist.
+
+```php
+$values = self::getRowValuesFromTable('MyDataObject', ['FieldName', 'OtherField'], 15);
+var_dump($values);
+
+// output
+array {
+	'FieldName' => 'Foo'
+	'OtherField' => '123'
+}
+```
+
+### `(boolean) setRowValuesOnTable(string $table, array $values, int $id)`
+
+Sets the values for multiple rows on a database table by the ID column. Useful when fields have been removed from the class' `$db` property, and therefore are no longer accessible through the ORM. Returns false if the table or any of the rows do not exist. Returns true if the SQL query was executed.
+
+```php
+self::setRowValuesOnTable('MyDataObject', [
+	'FieldName' => 'Bar', 
+	'OtherField' => 321
+], 15);
+```
+
