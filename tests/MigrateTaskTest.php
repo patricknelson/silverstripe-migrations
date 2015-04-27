221fc21c663eb75d541f6fb62a7deaa1df1ae003
Patrick Nelson <patnelson@ebay.com>
2015-04-27 00:30:59 -0400

    Added unit tests for rolling back batches of migrations in the proper order.

--- tests/MigrateTaskTest.php
+++ tests/MigrateTaskTest.php
@@ -2,10 +2,58 @@
 
 class MigrateTaskTest extends SapphireTest {
 
+	protected static $fixture_file = 'MigrateTaskTest.yml';
+
+	/**
+	 * @var MigrateTask
+	 */
+	protected $task;
+
 	public function testEnsureWorking() {
 		// Just a generic test to make sure unit testing is working.
 		$task = new MigrateTask();
 		$this->assertContains("migrations", $task->getMigrationPath());
+	}
+
+	protected function getTask() {
+		if (isset($this->task)) return $this->task;
+		return $this->task = new MigrateTask();
+	}
+
+
+	public function testLatestBatch() {
+		// Ensure that there are 2 migrations in the latest batch and 1 before that and that they both will run in the correct order.
+		$this->assertEquals(2, MigrateTask::getLatestBatch());
+		$this->getTask()->down();
+		$this->assertEquals(1, MigrateTask::getLatestBatch());
+	}
+
+
+	public function testTransactionRollback() {
+		// TODO: Ensure changes are rolled back in case there is an exception.
+	}
+
+}
+
+
+class Migration_UnitTestOnly implements TestOnly {
+
+	public static $throwException = false;
+
+	public function up() {
+		// TODO: Do stuff here.
+
+		static::exceptionator();
+	}
+
+	public function down() {
+		// TODO: Reverse stuff here.
+
+		static::exceptionator();
+	}
+
+	protected static function exceptionator() {
+		if (static::$throwException) throw new Exception("Test exception.");
 	}
 
 }
